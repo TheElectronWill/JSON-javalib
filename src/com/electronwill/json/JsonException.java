3d24611d4a27f37b87b54b8f835f64d78b7dc65b
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-03-31 21:40:59 +0200

    Remove wrong license header

--- src/com/electronwill/json/JsonException.java
+++ src/com/electronwill/json/JsonException.java
@@ -1,17 +1,3 @@
-/*
- * Copyright (C) 2015 ElectronWill
- *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
- * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Affero General Public License along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
- */
 package com.electronwill.json;
 
 /**
@@ -23,35 +9,16 @@ public class JsonException extends RuntimeException {
 	
 	private static final long serialVersionUID = 1L;
 	
-	/**
-	 * Creates an empty JSONExceptipon.
-	 */
 	public JsonException() {}
 	
-	/**
-	 * Creates a new JSONException with the given message.
-	 *
-	 * @param message the error message
-	 */
 	public JsonException(String message) {
 		super(message);
 	}
 	
-	/**
-	 * Creates a new JSONException with the given cause.
-	 *
-	 * @param cause the cause of this exception
-	 */
 	public JsonException(Throwable cause) {
 		super(cause);
 	}
 	
-	/**
-	 * Creates a new JSONException with the given message and cause.
-	 *
-	 * @param message the error message
-	 * @param cause the cause
-	 */
 	public JsonException(String message, Throwable cause) {
 		super(message, cause);
 	}
